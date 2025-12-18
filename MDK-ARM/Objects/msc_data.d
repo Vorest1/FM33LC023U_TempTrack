891dcdba2b798a5112586a460cf498fad4d1311f
Vorest1 <148646056+Vorest1@users.noreply.github.com>
2025-12-18 20:54:20 +0300

    Delete MDK-ARM/Objects directory

--- MDK-ARM/Objects/msc_data.d
+++
@@ -1,5 +0,0 @@
-.\objects\msc_data.o: ..\Middleware\USB\App\src\msc_data.c
-.\objects\msc_data.o: ..\Middleware\USB\App\inc\msc_data.h
-.\objects\msc_data.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\msc_data.o: ..\Middleware\USB\App\inc\usbd_conf.h
-.\objects\msc_data.o: ..\Middleware\USB\App\inc\usb_conf.h
